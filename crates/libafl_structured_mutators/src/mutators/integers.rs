64702b346b36ea16a79a1f0e3e089e653440e9b5
Ely Ronnen <elyronnen@gmail.com>
2025-09-21 14:48:34 +0200

    implement more integer mutators: bitflip, byteflip, randombye, rnegate...

--- crates/libafl_structured_mutators/src/mutators/integers.rs
+++ crates/libafl_structured_mutators/src/mutators/integers.rs
@@ -1,3 +1,5 @@
+use core::mem::size_of;
+
 use libafl::mutators::mutations::{INTERESTING_8, INTERESTING_16, INTERESTING_32};
 use libafl_bolts::rands::Rand;
 
@@ -130,6 +132,151 @@ impl_int_interesting_mutator!(I32InterestingMutator, i32, INTERESTING_32);
 impl_int_interesting_mutator!(U64InterestingMutator, u64, INTERESTING_64);
 impl_int_interesting_mutator!(I64InterestingMutator, i64, INTERESTING_64);
 
+macro_rules! impl_int_bit_flip_mutator {
+    ($mutator_name:ident, $name:ty) => {
+        #[derive(Default, Debug)]
+        pub struct $mutator_name;
+
+        impl<S> StructuredMutator<$name, S> for $mutator_name
+        where
+            S: libafl::state::HasRand,
+        {
+            fn mutate(&mut self, data: &mut $name, state: &mut S) -> bool {
+                let bit_pos = state.rand_mut().below_or_zero(size_of::<$name>() * 8);
+                *data ^= 1 << bit_pos;
+                true
+            }
+        }
+    };
+}
+
+macro_rules! impl_int_byte_flip_mutator {
+    ($mutator_name:ident, $name:ty) => {
+        #[derive(Default, Debug)]
+        pub struct $mutator_name;
+
+        impl<S> StructuredMutator<$name, S> for $mutator_name
+        where
+            S: libafl::state::HasRand,
+        {
+            fn mutate(&mut self, data: &mut $name, state: &mut S) -> bool {
+                let byte_pos = state.rand_mut().below_or_zero(size_of::<$name>());
+                *data ^= (0xffu8 as $name) << (byte_pos * 8);
+                true
+            }
+        }
+    };
+}
+
+macro_rules! impl_int_byte_neg_mutator {
+    ($mutator_name:ident, $name:ty) => {
+        #[derive(Default, Debug)]
+        pub struct $mutator_name;
+
+        impl<S> StructuredMutator<$name, S> for $mutator_name
+        where
+            S: libafl::state::HasRand,
+        {
+            fn mutate(&mut self, data: &mut $name, state: &mut S) -> bool {
+                let byte_pos = state.rand_mut().below_or_zero(size_of::<$name>());
+                let mask = (0xffu8 as $name) << (byte_pos * 8);
+                let selected_byte = (*data & mask) >> (byte_pos * 8);
+                let negated = (!selected_byte & 0xffu8 as $name) as $name;
+                *data = (*data & !mask) | (negated << (byte_pos * 8));
+                true
+            }
+        }
+    };
+}
+
+macro_rules! impl_int_byte_rand_mutator {
+    ($mutator_name:ident, $name:ty) => {
+        #[derive(Default, Debug)]
+        pub struct $mutator_name;
+
+        impl<S> StructuredMutator<$name, S> for $mutator_name
+        where
+            S: libafl::state::HasRand,
+        {
+            fn mutate(&mut self, data: &mut $name, state: &mut S) -> bool {
+                let byte_pos = state.rand_mut().below_or_zero(size_of::<$name>());
+                let rand_byte = state.rand_mut().below_or_zero(256) as $name;
+                let mask = (0xffu8 as $name) << (byte_pos * 8);
+                *data = (*data & !mask) | (rand_byte << (byte_pos * 8));
+                true
+            }
+        }
+    };
+}
+
+macro_rules! impl_int_byte_add_mutator {
+    ($mutator_name:ident, $name:ty) => {
+        #[derive(Default, Debug)]
+        pub struct $mutator_name;
+
+        impl<S> StructuredMutator<$name, S> for $mutator_name
+        where
+            S: libafl::state::HasRand,
+        {
+            fn mutate(&mut self, data: &mut $name, state: &mut S) -> bool {
+                let byte_pos = state.rand_mut().below_or_zero(size_of::<$name>());
+                let add_val = state.rand_mut().below_or_zero(256) as $name;
+                let mask = (0xffu8 as $name) << (byte_pos * 8);
+                let selected_byte = (*data & mask) >> (byte_pos * 8);
+                let added = (selected_byte.wrapping_add(add_val) & 0xffu8 as $name) as $name;
+                *data = (*data & !mask) | (added << (byte_pos * 8));
+                true
+            }
+        }
+    };
+}
+
+// Implement new mutators for each integer type
+impl_int_bit_flip_mutator!(U8BitFlipMutator, u8);
+impl_int_bit_flip_mutator!(I8BitFlipMutator, i8);
+impl_int_bit_flip_mutator!(U16BitFlipMutator, u16);
+impl_int_bit_flip_mutator!(I16BitFlipMutator, i16);
+impl_int_bit_flip_mutator!(U32BitFlipMutator, u32);
+impl_int_bit_flip_mutator!(I32BitFlipMutator, i32);
+impl_int_bit_flip_mutator!(U64BitFlipMutator, u64);
+impl_int_bit_flip_mutator!(I64BitFlipMutator, i64);
+
+impl_int_byte_flip_mutator!(U8ByteFlipMutator, u8);
+impl_int_byte_flip_mutator!(I8ByteFlipMutator, i8);
+impl_int_byte_flip_mutator!(U16ByteFlipMutator, u16);
+impl_int_byte_flip_mutator!(I16ByteFlipMutator, i16);
+impl_int_byte_flip_mutator!(U32ByteFlipMutator, u32);
+impl_int_byte_flip_mutator!(I32ByteFlipMutator, i32);
+impl_int_byte_flip_mutator!(U64ByteFlipMutator, u64);
+impl_int_byte_flip_mutator!(I64ByteFlipMutator, i64);
+
+impl_int_byte_neg_mutator!(U8ByteNegMutator, u8);
+impl_int_byte_neg_mutator!(I8ByteNegMutator, i8);
+impl_int_byte_neg_mutator!(U16ByteNegMutator, u16);
+impl_int_byte_neg_mutator!(I16ByteNegMutator, i16);
+impl_int_byte_neg_mutator!(U32ByteNegMutator, u32);
+impl_int_byte_neg_mutator!(I32ByteNegMutator, i32);
+impl_int_byte_neg_mutator!(U64ByteNegMutator, u64);
+impl_int_byte_neg_mutator!(I64ByteNegMutator, i64);
+
+impl_int_byte_rand_mutator!(U8ByteRandMutator, u8);
+impl_int_byte_rand_mutator!(I8ByteRandMutator, i8);
+impl_int_byte_rand_mutator!(U16ByteRandMutator, u16);
+impl_int_byte_rand_mutator!(I16ByteRandMutator, i16);
+impl_int_byte_rand_mutator!(U32ByteRandMutator, u32);
+impl_int_byte_rand_mutator!(I32ByteRandMutator, i32);
+impl_int_byte_rand_mutator!(U64ByteRandMutator, u64);
+impl_int_byte_rand_mutator!(I64ByteRandMutator, i64);
+
+impl_int_byte_add_mutator!(U8ByteAddMutator, u8);
+impl_int_byte_add_mutator!(I8ByteAddMutator, i8);
+impl_int_byte_add_mutator!(U16ByteAddMutator, u16);
+impl_int_byte_add_mutator!(I16ByteAddMutator, i16);
+impl_int_byte_add_mutator!(U32ByteAddMutator, u32);
+impl_int_byte_add_mutator!(I32ByteAddMutator, i32);
+impl_int_byte_add_mutator!(U64ByteAddMutator, u64);
+impl_int_byte_add_mutator!(I64ByteAddMutator, i64);
+
 /// Implements a default mutator for integers that chooses a random basic integer mutation each time.
 macro_rules! impl_int_default_mutator {
     ($mutator_name:ident, $name:ty, $($inner:ty),* $(,)?) => {
@@ -185,6 +332,11 @@ impl_int_default_mutator!(
     U8IncMutator,
     U8DecMutator,
     U8InterestingMutator,
+    U8BitFlipMutator,
+    U8ByteFlipMutator,
+    U8ByteNegMutator,
+    U8ByteRandMutator,
+    U8ByteAddMutator,
 );
 impl_int_default_mutator!(
     I8StructuredMutator,
@@ -192,6 +344,11 @@ impl_int_default_mutator!(
     I8IncMutator,
     I8DecMutator,
     I8InterestingMutator,
+    I8BitFlipMutator,
+    I8ByteFlipMutator,
+    I8ByteNegMutator,
+    I8ByteRandMutator,
+    I8ByteAddMutator,
 );
 impl_int_default_mutator!(
     U16StructuredMutator,
@@ -199,6 +356,11 @@ impl_int_default_mutator!(
     U16IncMutator,
     U16DecMutator,
     U16InterestingMutator,
+    U16BitFlipMutator,
+    U16ByteFlipMutator,
+    U16ByteNegMutator,
+    U16ByteRandMutator,
+    U16ByteAddMutator,
 );
 impl_int_default_mutator!(
     I16StructuredMutator,
@@ -206,6 +368,11 @@ impl_int_default_mutator!(
     I16IncMutator,
     I16DecMutator,
     I16InterestingMutator,
+    I16BitFlipMutator,
+    I16ByteFlipMutator,
+    I16ByteNegMutator,
+    I16ByteRandMutator,
+    I16ByteAddMutator,
 );
 impl_int_default_mutator!(
     U32StructuredMutator,
@@ -213,6 +380,11 @@ impl_int_default_mutator!(
     U32IncMutator,
     U32DecMutator,
     U32InterestingMutator,
+    U32BitFlipMutator,
+    U32ByteFlipMutator,
+    U32ByteNegMutator,
+    U32ByteRandMutator,
+    U32ByteAddMutator,
 );
 impl_int_default_mutator!(
     I32StructuredMutator,
@@ -220,6 +392,11 @@ impl_int_default_mutator!(
     I32IncMutator,
     I32DecMutator,
     I32InterestingMutator,
+    I32BitFlipMutator,
+    I32ByteFlipMutator,
+    I32ByteNegMutator,
+    I32ByteRandMutator,
+    I32ByteAddMutator,
 );
 impl_int_default_mutator!(
     U64StructuredMutator,
@@ -227,6 +404,11 @@ impl_int_default_mutator!(
     U64IncMutator,
     U64DecMutator,
     U64InterestingMutator,
+    U64BitFlipMutator,
+    U64ByteFlipMutator,
+    U64ByteNegMutator,
+    U64ByteRandMutator,
+    U64ByteAddMutator,
 );
 impl_int_default_mutator!(
     I64StructuredMutator,
@@ -234,4 +416,9 @@ impl_int_default_mutator!(
     I64IncMutator,
     I64DecMutator,
     I64InterestingMutator,
+    I64BitFlipMutator,
+    I64ByteFlipMutator,
+    I64ByteNegMutator,
+    I64ByteRandMutator,
+    I64ByteAddMutator,
 );
